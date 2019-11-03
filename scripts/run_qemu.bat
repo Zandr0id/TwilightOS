000b12f2e6c15864290e21b9250a1177924fa157
Zane Youmans <zyoumans@TheBatComputer.localdomain>
2019-11-02 22:29:34 -0700

    run_qemu.bat: Changed the path, because I moved the folder

--- scripts/run_qemu.bat
+++ scripts/run_qemu.bat
@@ -2,4 +2,4 @@ rem Go run the qemu program with the kernel
 rem Remember to fix this if the file path changes.
 
 cd \Installed_Programs\qemu
-qemu-system-i386.exe -kernel "\Engineering\OS project\Repos\TwilightOS\bin\kernel.bin"
+qemu-system-i386.exe -kernel "\Programming\OS\TwilightOS\bin\kernel.bin"
