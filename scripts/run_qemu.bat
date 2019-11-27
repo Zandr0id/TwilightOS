904b1db2ba3e2824c1a50a402c366c2612f50b4d
Zane Youmans <zyoumans@TheBatComputer.localdomain>
2019-11-26 18:19:41 -0700

    Fixed the path to my installation of Qemu
    Also removed the gdb flags because I can't get it to work for now.

--- scripts/run_qemu.bat
+++ scripts/run_qemu.bat
@@ -1,7 +1,7 @@
 rem Go run the qemu program with the kernel
 rem The windows command prompt will use the same working directory as we ran make from
 
-set QEMU="C:\Installed_Programs\qemu\qemu-system-i386.exe"
+set QEMU="D:\Installed_Programs\qemu\qemu-system-i386.exe"
 
-%QEMU% -kernel "bin\kernel.bin" -S -s -serial stdio
+%QEMU% -kernel "bin\kernel.bin" -serial stdio
 rem %QEMU% -kernel "bin\kernel.bin" -serial stdio
