72910d600e7604fa2e37feb9c8dea3631cce97ee
Zane Youmans <zyoumans@TheBatComputer.localdomain>
2019-11-29 22:32:32 -0700

    Memory.cpp: Added an array version of New

--- src/kernel/libc/memory.cpp
+++ src/kernel/libc/memory.cpp
@@ -190,6 +190,11 @@ void * operator new (size_t size)
     return malloc(size);
 }
 
+void * operator new[] (size_t size)
+{
+    return malloc(size);
+}
+
 void operator delete (void* ptr)
 {
     free(ptr);
